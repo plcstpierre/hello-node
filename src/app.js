9ef2dfc15920fc683fff4dde6852a022e9c96bc6
Pier-Luc Caron St-Pierre <pier-luccaronst-pierre@motivateco.com>
2018-02-12 20:52:11 -0500

    Add unhandled promise rejection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ require('dotenv').config()
 
 const logger = require('./logger')
 
+process.on("unhandledRejection", err => {
+ logger.error({
+   "message": "Unhandled Rejection",
+   "err": err,
+ })
+})
+
 logger.info({
     hello: 'world'
 }, 'hi!')
